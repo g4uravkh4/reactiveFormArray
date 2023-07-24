42fc8afcca05ca481192c62fb5944d96e1230c26
aura-gv <gauravkhatiwada4@gmail.com>
2023-07-24 12:51:53 +0545

    gg

--- src/app/my-form-component/my-form-component.component.ts
+++ src/app/my-form-component/my-form-component.component.ts
@@ -14,13 +14,14 @@ import {
 })
 export class MyFormComponentComponent {
   simpleForm: FormGroup;
+  subjectsArray: any;
 
   constructor(private fb: FormBuilder) {
     this.simpleForm = this.fb.group({
-      name: '',
-      class: '',
-      roll: '',
-      email: '',
+      name: ['', Validators.required],
+      class: ['', Validators.required],
+      roll: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
 
       subjects: this.fb.array([]),
     });
@@ -48,5 +49,15 @@ export class MyFormComponentComponent {
 
   onSubmit() {
     console.log(this.simpleForm.value);
+  }
+
+  onReset() {
+    this.simpleForm.reset({
+      subName: '',
+      subMarks: '',
+    });
+  }
+  onDeleteAll() {
+    this.subjects().clear();
   }
 }
